cce43309de99a3b4ce6197966e51d4df7b8a8258
kkgosu <kostya96.08@mail.ru>
2020-07-19 17:01:35 +0300

    added all fragments with DesignSystem colors, dimens, styles

---
+++ app/src/main/java/com/kvlg/runningtracker/ui/fragments/StatisticFragment.kt
@@ -0,0 +1,11 @@
+package com.kvlg.runningtracker.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.kvlg.runningtracker.R
+
+/**
+ * @author Konstantin Koval
+ * @since 19.07.2020
+ */
+class StatisticFragment: Fragment(R.layout.fragment_statistics) {
+}
---
+++ app/src/main/java/com/kvlg/runningtracker/ui/fragments/TrackingFragment.kt
@@ -0,0 +1,11 @@
+package com.kvlg.runningtracker.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.kvlg.runningtracker.R
+
+/**
+ * @author Konstantin Koval
+ * @since 19.07.2020
+ */
+class TrackingFragment : Fragment(R.layout.fragment_tracking) {
+}
