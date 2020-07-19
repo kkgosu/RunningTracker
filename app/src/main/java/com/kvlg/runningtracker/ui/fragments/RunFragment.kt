348f5dfcb5fa9961f49e9dfa9b86333664bea266
kkgosu <kostya96.08@mail.ru>
2020-07-19 17:04:09 +0300

    added viewmodels and injection fragment

--- app/src/main/java/com/kvlg/runningtracker/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/kvlg/runningtracker/ui/fragments/RunFragment.kt
@@ -1,11 +1,16 @@
 package com.kvlg.runningtracker.ui.fragments
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.kvlg.runningtracker.R
+import com.kvlg.runningtracker.ui.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
 /**
  * @author Konstantin Koval
  * @since 19.07.2020
  */
+@AndroidEntryPoint
 class RunFragment : Fragment(R.layout.fragment_run) {
+    private val viewModel: MainViewModel by viewModels()
 }
--- app/src/main/java/com/kvlg/runningtracker/ui/fragments/StatisticFragment.kt
+++ app/src/main/java/com/kvlg/runningtracker/ui/fragments/StatisticFragment.kt
@@ -1,11 +1,16 @@
 package com.kvlg.runningtracker.ui.fragments
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.kvlg.runningtracker.R
+import com.kvlg.runningtracker.ui.viewmodels.StatisticsViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
 /**
  * @author Konstantin Koval
  * @since 19.07.2020
  */
-class StatisticFragment: Fragment(R.layout.fragment_statistics) {
+@AndroidEntryPoint
+class StatisticFragment : Fragment(R.layout.fragment_statistics) {
+    private val viewModel: StatisticsViewModel by viewModels()
 }
