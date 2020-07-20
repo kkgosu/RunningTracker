7149f7bd7d217db28736d1456bea554fac2db75a
kkgosu <kostya96.08@mail.ru>
2020-07-21 00:03:08 +0300

    added navigtaion

--- app/src/main/java/com/kvlg/runningtracker/ui/MainActivity.kt
+++ app/src/main/java/com/kvlg/runningtracker/ui/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.kvlg.runningtracker.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.navigation.NavController
 import com.kvlg.runningtracker.R
+import com.kvlg.runningtracker.databinding.ActivityMainBinding
+import com.kvlg.runningtracker.utils.setupWithNavController
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var currentNavController: LiveData<NavController>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navGraphIds = intArrayOf(
+            R.navigation.runs,
+            R.navigation.statistics,
+            R.navigation.settings
+        )
+
+        val controller = binding.bnv.setupWithNavController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.nav_host_fragment,
+            intent = intent
+        )
+
+        currentNavController = controller
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return currentNavController?.value?.navigateUp() ?: false
     }
 }
