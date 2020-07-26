3895e83c0c457072dc44fe5aee3672b95e25964f
kkgosu <kostya96.08@mail.ru>
2020-07-26 19:09:57 +0300

    notification + handling notification click

--- app/src/main/java/com/kvlg/runningtracker/services/TrackingService.kt
+++ app/src/main/java/com/kvlg/runningtracker/services/TrackingService.kt
@@ -1,7 +1,18 @@
 package com.kvlg.runningtracker.services
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.NotificationManager.IMPORTANCE_LOW
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.content.getSystemService
 import androidx.lifecycle.LifecycleService
+import com.kvlg.runningtracker.R
+import com.kvlg.runningtracker.ui.MainActivity
 import com.kvlg.runningtracker.utils.Constants
 import timber.log.Timber
 
@@ -11,11 +22,18 @@ import timber.log.Timber
  */
 class TrackingService : LifecycleService() {
 
+    var isFirstRun = true
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
             when (it.action) {
                 Constants.ACTION_START_OR_RESUME_SERVICE -> {
-                    Timber.d("Started or resumed service")
+                    if (isFirstRun) {
+                        startForegroundService()
+                        isFirstRun = false
+                    } else {
+                        Timber.d("Resuming service..")
+                    }
                 }
                 Constants.ACTION_PAUSE_SERVICE -> {
                     Timber.d("Paused service")
@@ -28,4 +46,37 @@ class TrackingService : LifecycleService() {
         return super.onStartCommand(intent, flags, startId)
     }
 
+    private fun startForegroundService() {
+        val notificationManager = getSystemService<NotificationManager>()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationManager?.let {
+                createNotificationChannel(notificationManager)
+            }
+        }
+
+        val notificationBuilder = NotificationCompat.Builder(this, Constants.NOTIFICATION_CHANNEL_ID)
+            .setAutoCancel(false)
+            .setOngoing(true)
+            .setSmallIcon(R.drawable.ic_run_24)
+            .setContentTitle(getString(R.string.app_name))
+            .setContentText(getString(R.string._00_00_00))
+            .setContentIntent(getMainActivityPendingIntent())
+
+        startForeground(Constants.NOTIFICATION_ID, notificationBuilder.build())
+    }
+
+    private fun getMainActivityPendingIntent() = PendingIntent.getActivity(
+        this,
+        0,
+        Intent(this, MainActivity::class.java).also {
+            it.action = Constants.ACTION_SHOW_TRACKING_FRAGMENT
+        }, FLAG_UPDATE_CURRENT
+    )
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(notificationManager: NotificationManager) {
+        val channel = NotificationChannel(Constants.NOTIFICATION_CHANNEL_ID, Constants.NOTIFICATION_CHANNEL_NAME, IMPORTANCE_LOW)
+        notificationManager.createNotificationChannel(channel)
+    }
 }
--- app/src/main/java/com/kvlg/runningtracker/ui/MainActivity.kt
+++ app/src/main/java/com/kvlg/runningtracker/ui/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.kvlg.runningtracker.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LiveData
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.kvlg.runningtracker.R
 import com.kvlg.runningtracker.databinding.ActivityMainBinding
+import com.kvlg.runningtracker.utils.Constants
 import com.kvlg.runningtracker.utils.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,6 +23,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        navigateToTrackingFragmentIfNeeded(intent)
 
         setSupportActionBar(binding.toolbar)
 
@@ -41,5 +46,16 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         return currentNavController?.value?.navigateUp() ?: false
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navigateToTrackingFragmentIfNeeded(intent)
+    }
+
+    private fun navigateToTrackingFragmentIfNeeded(intent: Intent?) {
+        if (intent?.action == Constants.ACTION_SHOW_TRACKING_FRAGMENT) {
+            binding.navHostFragment.findNavController().navigate(R.id.action_global_trackingFragment)
+        }
     }
 }
