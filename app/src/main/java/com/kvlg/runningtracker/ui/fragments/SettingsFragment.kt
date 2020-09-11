175181b975e2ec142140f452b668d8f3ce8250d1
kkgosu <kostya96.08@mail.ru>
2020-09-11 23:27:35 +0300

    removed old settings fragment

--- app/src/main/java/com/kvlg/runningtracker/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/kvlg/runningtracker/ui/fragments/SettingsFragment.kt
@@ -1,32 +1,23 @@
 package com.kvlg.runningtracker.ui.fragments
 
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import com.google.android.material.snackbar.Snackbar
-import com.kvlg.runningtracker.R
 import com.kvlg.runningtracker.databinding.FragmentSettingsBinding
-import com.kvlg.runningtracker.ui.fragments.common.InfoWeightDialog
-import com.kvlg.runningtracker.utils.Constants
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 /**
  * @author Konstantin Koval
- * @since 19.07.2020
+ * @since 26.08.2020
  */
 @AndroidEntryPoint
 class SettingsFragment : Fragment() {
-
     private var _binding: FragmentSettingsBinding? = null
-    private val binding get() = _binding!!
-
-    @Inject
-    lateinit var sharedPreferences: SharedPreferences
+    private val binding: FragmentSettingsBinding
+        get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,43 +25,21 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
-        activity?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        loadFieldsFromSharedPref()
-        binding.applyChangesButton.setOnClickListener {
-            val isSuccess = applyChangesToSharedPref()
-            if (isSuccess) {
-                Snackbar.make(view, getString(R.string.saved_settings_snackbar_text), Snackbar.LENGTH_LONG).show()
-            } else {
-                Snackbar.make(view, getString(R.string.error_save_settings_snackbar_text), Snackbar.LENGTH_LONG).show()
+        (requireActivity() as AppCompatActivity).apply {
+            setSupportActionBar(binding.toolbar)
+            supportActionBar?.run {
+                setDisplayHomeAsUpEnabled(true)
             }
-        }
-        binding.weightInputLayout.setEndIconOnClickListener {
-            InfoWeightDialog().show(parentFragmentManager, InfoWeightDialog.TAG)
+            title = ""
         }
     }
 
-    private fun loadFieldsFromSharedPref() {
-        val name = sharedPreferences.getString(Constants.KEY_PREF_NAME, "") ?: ""
-        val weight = sharedPreferences.getFloat(Constants.KEY_PREF_WEIGHT, 80f)
-        binding.nameEditText.setText(name)
-        binding.weightEditText.setText(if (weight - weight.toInt() > 0) weight.toString() else weight.toInt().toString())
-    }
-
-    private fun applyChangesToSharedPref(): Boolean {
-        val nameText = binding.nameEditText.text.toString()
-        val weightText = binding.weightEditText.text.toString()
-        if (nameText.isEmpty() || weightText.isEmpty()) {
-            return false
-        }
-        sharedPreferences.edit().apply {
-            putString(Constants.KEY_PREF_NAME, nameText)
-            putFloat(Constants.KEY_PREF_WEIGHT, weightText.toFloat())
-        }.apply()
-
-        return true
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
     }
 }
